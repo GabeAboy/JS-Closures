5487ae68034f9e2c4d8635e86d14dc2e7b9e368b
Merksue <gabeaboy@gmail.com>
2016-11-09 22:42:23 -0500

    Closures/Self Invoked Functions

--- closures.js
+++ closures.js
@@ -6,7 +6,7 @@ var outer = function(){
   var name = 'Tyler';
   return function(){
     return 'The original name was ' + name;
-  }
+  };
 };
 
 /****** INSTRUCTIONS PROBLEM 1 ******/
@@ -15,19 +15,10 @@ closure over the name variable. Invoke outer saving the return value into
 another variable called 'inner'. */
 
 // Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
-
-//Code Here
-
-
-
-
-
-
-
-
-
+console.log(inner());
 
 /******************************************************************************\
 	#PROBLEM-02
@@ -39,8 +30,10 @@ var callFriend = function(){
   function callF(number){
     return 'Calling ' + friend + ' at ' + number;
   }
+
   return callF;
 };
+
 
 /****** INSTRUCTIONS PROBLEM 2 ******/
 /* Above you're given a callFriend function that returns another function.
@@ -48,15 +41,10 @@ Create a makeCall function that when invoked logs 'Calling Jake at 435-215-9248'
 in your console. */
 
   //Code Here
-
-
-
-
-
-
-
-
-
+  function makeCall(number) {
+    return callFriend()(number);
+  }
+  console.log(makeCall("435-215-9248"));
 
 /******************************************************************************\
 	#PROBLEM-03
@@ -68,19 +56,18 @@ properly. */
 
 //Code Here
 
+function makeCounter() {
+var x = 1;
+  return function (){
+    return x++;
+  };
+}
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
-
-
-
-
-
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -97,24 +84,19 @@ the module pattern to achieve this. */
 
 function counterFactory(value) {
 
-  // Code here.
-
-
   return {
-  }
+    inc: function(){
+      ++value;
+      return value;
+    },
+    dec: function(){
+      --value;
+      return value;
+    }
+
+  };
 }
-
-
 counter = counterFactory(10);
-
-
-
-
-
-
-
-
-
 
 /******************************************************************************\
 	#PROBLEM-05
@@ -129,14 +111,14 @@ function motivation(firstname, lastname){
   var welcomeText = 'You\'re doing awesome, keep it up ';
 
   // code message function here.
-
-
+  function message() {
+    return welcomeText+firstname+" "+lastname+".";
+  }
   //Uncommment this to return the value of your invoked message function
-  //return message();
-
+  return message();
 }
 
-motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
+console.log(motivation('Billy', 'Bob')); // 'Your doing awesome keep it up Billy Bob
 
 
 
@@ -171,23 +153,16 @@ var module = (function() {
 	// outside our lexical scope
 
   return {
+    publicMethod:function(){
+      return privateMethod();
+    }
     // Code here.
   };
 
 })();
 
 // Uncomment this after you create your public method
-//   module.publicMethod();
-
-
-
-
-
-
-
-
-
-
+  module.publicMethod();
 /******************************************************************************\
 	#PROBLEM-07
 \******************************************************************************/
@@ -195,42 +170,44 @@ var module = (function() {
 /****** INSTRUCTIONS PROBLEM 7 ******/
 /* Here we have a for loop that will iterate as long as i is less than or equal
 to 5. What we need to do is console.log(i) so that it logs ( 0 then 1 then 2
-then 3, etc). Run this code in your console to see what the output is. */
+then 3, etc). Run this code in your console to see what the output is.
+
+(function( i ){
+ funcArray.push(
+   function(){ return i; }
+ );
+})( i );
+*/
 
 // To make this code work you will need to create a new scope for every iteration.
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000)
-  }
+//self invked func iife
+    (function(i){
+      setTimeout(function() {
+        console.log(i);
+      }, i * 1000);
+    })(i);
 
-  function newScope(i) {
-    console.log(i)
   }
 }
 timeOutCounter();
 
-
-
-
-
-
 /******************************************************************************\
 	#PROBLEM-08
 \******************************************************************************/
-
+console.log("HUNJNIJJIJNJNIJNJINJNI");
 var funcArray = [];
-
-/*
-  Make the following code work
-
-  funcArray[0]() //0
-  funcArray[1]() //1
-  funcArray[2]() //2
-  funcArray[3]() //3
-  funcArray[4]() //4
-  funcArray[5]() //5
-
-  *Hint: Don't let this fool you. Break down what's really happening here.
-*/
+for( var i = 0; i <=5; i++ ){
+ 	(function( i ){
+ 		funcArray.push(
+ 			function(){ return i; }
+ 		);
+ 	})( i );
+ }
+  funcArray[0](); //0
+  funcArray[1](); //1
+  funcArray[2](); //2
+  funcArray[3](); //3
+  funcArray[4](); //4
+  funcArray[5](); //5
